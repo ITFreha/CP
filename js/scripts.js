a6d09e85dc01955d19903b5b16fa635a6ce6fb35
AlexeenkoRodion@yandex.by <AlexeenkoRodion@yandex.by>
2017-09-20 20:30:00 +0300

    changed menu for pc

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 function changeMenu() {
 	if (window.matchMedia("(max-width: 767px)").matches) {
 		//mob
-		$("#menu").removeClass('col-sm-2 col-sm-offset-1 menu-left');
-		$("#menu ul").removeClass('nav nav-stacked');
+		$("#menu").removeClass('menu-left');
 
 		$("#menu").attr('class', 'mob-menu');
 	} else {
 		//pc
 		$("#menu").removeClass('mob-menu');
 
-		$("#menu").attr('class', 'col-sm-2 col-sm-offset-1 menu-left');
-		$("#menu ul").attr('class', 'nav nav-stacked');
+		$("#menu").attr('class', 'menu-left');
 	}
 }
 

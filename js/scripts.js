324e5fca5bc6350f2264b5a0ec47df0ca1eee563
AlexeenkoRodion@yandex.by <AlexeenkoRodion@yandex.by>
2017-10-10 21:02:34 +0300

    ban possibility to change scale, deleted menu in horizontal orientation on mobiles

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,18 @@ function changeMenu() {
 		$("#menu").removeClass('menu-left');
 
 		$("#menu").attr('class', 'mob-menu');
+
+		//alert(window.screen.availWidth + " " + window.screen.availHeight);
+
+		if (window.screen.availWidth > window.screen.availHeight) {
+			$('#menu').css('display', 'none');
+			$('#navbarTop').css('display', 'none');
+			$('body').css('padding-top', '0px');
+		} else {
+			$("#menu").css('display', 'block');
+			$("#navbarTop").css('display', 'block');
+			$('body').css('padding-top', '50px');
+		}
 	} else {
 		//pc
 		$("#menu").removeClass('mob-menu');
